6a8c0c7ab8ca5e8e54c09492f1312cf86f414895
Nagendra <nagendra.shukla@gmail.com>
2019-06-12 22:15:24 +1000

    Updates

---
+++ precache-manifest.a381a98b3e073844d16cb7a5c5bb5e46.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "6edad17281b472dc6699aa1cfc9045eb",
+    "url": "/ReactSampleApp/static/media/heme.6edad172.jpg"
+  },
+  {
+    "revision": "0e72bff2fa68125fc8a7",
+    "url": "/ReactSampleApp/static/css/main.7bf1912c.chunk.css"
+  },
+  {
+    "revision": "eceade0440ecf67ba8c8",
+    "url": "/ReactSampleApp/static/js/1.eceade04.chunk.js"
+  },
+  {
+    "revision": "3af83eb51826d5cb3d31",
+    "url": "/ReactSampleApp/static/js/runtime~main.3af83eb5.js"
+  },
+  {
+    "revision": "e83ce5cdbc04ebc89686872772d97c5c",
+    "url": "/ReactSampleApp/static/media/plates-header.e83ce5cd.jpg"
+  },
+  {
+    "revision": "8bf3a19406582cb3f2112aa545434647",
+    "url": "/ReactSampleApp/static/media/blue-stripe-stoneware-plate.8bf3a194.jpg"
+  },
+  {
+    "revision": "7ff619d5a67100cdcf2e21894f2e3486",
+    "url": "/ReactSampleApp/static/media/hand-painted-blue-flat-dish.7ff619d5.jpg"
+  },
+  {
+    "revision": "0e72bff2fa68125fc8a7",
+    "url": "/ReactSampleApp/static/js/main.0e72bff2.chunk.js"
+  },
+  {
+    "revision": "b06521e69b554fe479f0f1e9989375d2",
+    "url": "/ReactSampleApp/static/media/bc-logo-bright.b06521e6.svg"
+  },
+  {
+    "revision": "e101361d960d4048862fbe740b261351",
+    "url": "/ReactSampleApp/static/media/plates-left.e101361d.png"
+  },
+  {
+    "revision": "001599dd45b27ee741facb6bf49d4a4c",
+    "url": "/ReactSampleApp/static/media/plates-right.001599dd.png"
+  },
+  {
+    "revision": "8c072928290b211b1fda5512bf8c20f1",
+    "url": "/ReactSampleApp/static/media/mashiko-yaki-green-small-plate.8c072928.jpg"
+  },
+  {
+    "revision": "60ec0c8207ca1315d0fd7bfa55abfe1c",
+    "url": "/ReactSampleApp/static/media/mashiko-yaki-indigo-small-plate.60ec0c82.jpg"
+  },
+  {
+    "revision": "7566c313be610d8b9e161de93476e5b4",
+    "url": "/ReactSampleApp/static/media/mashiko-yaki-saucer.7566c313.jpg"
+  },
+  {
+    "revision": "279a23ea334f22443d9d1f52d9542636",
+    "url": "/ReactSampleApp/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/ReactSampleApp/precache-manifest.a381a98b3e073844d16cb7a5c5bb5e46.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/ReactSampleApp/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
